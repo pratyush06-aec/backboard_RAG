bc3689052cb9245b83e305bf9a7244838f50d874
Pratyush Dutta <pratyush00710@gmail.com>
2026-04-16 20:31:31 +0530

    Made some changes in the code within the files, to make the backend logic properly functional

--- backboard.io_RAG/utils/config.py
+++ backboard.io_RAG/utils/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:
-    BACKBOARD_API_KEY = os.getenv("BACKBOARD_API_KEY")
+BACKBOARD_API_KEY = os.getenv("BACKBOARD_API_KEY")
 
-    DEBUG = os.getenv("DEBUG", "False") == "True"
-    APP_NAME = "Backboard Assistant"
+# Debugger (optional) 
+DEBUG = os.getenv("DEBUG", "False") == "True"
+APP_NAME = "Backboard Assistant"
